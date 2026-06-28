5d3953b82ee3ce036998ef075ffa0535b1cd2bde
aj0220 <65477006+aj0220@users.noreply.github.com>
2026-06-28 15:02:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 
 # Read data from the "Bills" worksheet
 st.write("Attempting to connect...")
+st.write("--- DEBUG INFO ---")
+# Get the connection config
+config = st.connection("gsheets")._instance.gsheets_connection_config
+st.write(config)
 df = conn.read(worksheet="Bills", ttl=5) 
 df = df.dropna(subset=['Card']).copy()
 
